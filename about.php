151e029f057ee4dac4143f827d18b60924e6e0bd
Jason Zheng <jasonz4200@gmail.com>
2019-07-25 15:10:16 -0500

    About page improvements

--- about.php
+++ about.php
@@ -1,11 +1,25 @@
-<?php
-
-@sqlStatement = "SELECT * FROM `table1` WHERE
-$result = $conn->query($sqlStatement);
-
-if($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "Name: ". $row['name'];
-    }
-}
-?>
+<?php include 'header.php'; ?>
+<div class='body'>
+    <div class='container'>
+        <h3>Why TestQuest?</h3>
+        <p>Duis ac arcu eu ipsum euismod pulvinar vitae tempus urna. Curabitur dolor massa, consectetur eget mauris sed,
+            ullamcorper hendrerit purus. Nunc gravida gravida tortor, ac accumsan mauris tincidunt vitae. Duis
+            scelerisque nisl in tortor commodo, eget auctor nunc euismod. Sed vulputate ante nec semper pretium. Vivamus
+            vestibulum vulputate enim. Proin a felis a diam pharetra efficitur in sit amet sapien. Fusce ultricies
+            lectus eu nisi aliquam, id condimentum lacus iaculis. Sed turpis ex, feugiat ac lorem quis, viverra varius
+            lorem. Maecenas aliquet dui finibus dignissim mollis. </p>
+        <br>
+        <h3>How do you compare to competitors (eg Quizlet)?</h3>
+        <p>We offer a much improved visual interface. Take a look at Quizlet's UI for editing flashcard. Look at all the
+            wasted space.</p>
+        <img src='img/quizlet.png' class='front-image'>
+        <br><br>
+        <h3>How hard did you guys even work on this?</h3>
+        <p>Very hard actually. We used a XAMPP stack alongside tools such as MySQL Workbench, PHPMyAdmin, Visual Studio
+            Code, and a multitude of languages including HTML, CSS, PHP, SQL, Javascript, jQuery, and a CSS framework
+            called Bootstrap. <b>We have added a total of 1329 lines of code and removed 550 lines of code.</b>
+        </p>
+        <img src='img/github.png' class='front-image'>
+    </div>
+</div>
+<?php include 'footer.php'; ?>
